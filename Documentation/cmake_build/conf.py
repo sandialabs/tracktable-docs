06ea7613757b9f2d1fb265ac0b8f2b50952ba3c2
Andy Wilson <atwilso@sandia.gov>
2025-05-16 21:30:14 -0600

    Remove deprecated get_html_theme_path() call
    Sphinx's RTD theme used to rely on this.  It is now deprecated.  The
    warning says this:
    
    WARNING: Calling get_html_theme_path is deprecated. If you are calling
    it to define html_theme_path, you are safe to remove that code.
    
    So be it!

--- Documentation/cmake_build/conf.py
+++ Documentation/cmake_build/conf.py
@@ -193,8 +193,8 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#import sphinx_rtd_theme
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
